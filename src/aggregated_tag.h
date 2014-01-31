f16c505157162d0e8cdc2958cd81f50ae1e84a01
ohtacaesar <ohtacaesar@gmail.com>
2014-01-31 19:00:40 +0900

    create aggregated_tag.h

---
+++ src/aggregated_tag.h
@@ -0,0 +1,12 @@
+#ifndef TAG_AGGREGATED_H
+#define TAG_AGGREGATED_H
+
+struct AggregatedTag {
+    char *name;
+    int total_view_count;
+    int total_comment_count;
+    int total_mylist_count;
+    int tag_size;
+};
+
+#endif
