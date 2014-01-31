1121b9daa47df74e04a630999e2dc97d0f987147
Takahiro Otomo <ohtacaesar@users.noreply.github.com>
2014-01-31 18:50:56 +0900

    implement Tag__create_fromCsvColumn(struct CsvColumn csv_column)

--- src/tag.h
+++ src/tag.h
@@ -1,6 +1,8 @@
 #ifndef TAG_H
 #define TAG_H
 
+#include "csv_column.h"
+
 struct Tag {
     char *name;
     int view_count;
@@ -8,6 +10,12 @@ struct Tag {
     int mylist_count;
 };
 
+// クラス的メソッド
 struct Tag* Tag__create(char *name, int view_count, int comment_count, int mylist_count);
+
+struct Tag* Tag__create_from_CsvColumn(struct CsvColumn *csv_column);
+
+// インスタンス的メソッド
+int Tag_delete(struct Tag *tag);
 
 #endif
