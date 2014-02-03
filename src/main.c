640da5d52431a7b4e6667bfc579a9de0698fbcf3
ohtacaesar <ohtacaesar@gmail.com>
2014-02-03 23:22:43 +0900

    bug fix in Files and update main to show csv files

--- src/main.c
+++ src/main.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "files.h"
 #include "tag.h"
 #include "tag_array.h"
 
 int main(int argc, char** argv) {
     if(argc < 2) {
-        fprintf(stderr, "Argment required. %s <csv_path>\n", argv[0]);
+        fprintf(stderr, "Argment required. %s <csv_directory_path>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    struct TagArray *tag_array = TagArray__create_from_csv(argv[1]);
-    if(tag_array == NULL) {
-        perror("TagArray__create_from_csv");
+    printf("%s\n", argv[1]);
+    struct Files *files = Files__create(argv[1]);
+    if(files == NULL) {
+        perror("Files__create");
         exit(EXIT_FAILURE);
     }
 
     int i;
-    for(i = 0; i < tag_array->size; i++) {
-        printf("%06d: %s\n", i, tag_array->array[i]->name);
+    for(i = 0; i < files->size; i++) {
+        printf("%s\n", files->file_paths[i]);
     }
 
-    TagArray_delete(tag_array);
+    Files_delete(files);
 
     return 0;
 }
