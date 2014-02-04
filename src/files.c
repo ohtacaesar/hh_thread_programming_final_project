d2c35221f3f77561bf0b960722bbd9bdf90a8173
Takahiro Otomo <ohtacaesar@users.noreply.github.com>
2014-02-05 05:15:02 +0900

    split Files__create to Files__create_with_file_path and Files__create_with_dir_path

--- src/files.c
+++ src/files.c
@@ -9,52 +9,67 @@
 
 #define DEFAULT_FILES_SIZE 512
 
-struct Files* Files__create(char *dir_path) {
+struct Files* Files__create_with_dir_path(char *path);
+struct Files* Files__create_with_file_path(char *path);
+
+struct Files* Files__create(char *path) {
+    struct stat statbuf;
+
     // remote tail / if exists
-    if(dir_path[strlen(dir_path) - 1] == '/') {
-        dir_path[strlen(dir_path) - 1] = '\0';
+    if(path[strlen(path) - 1] == '/') {
+        path[strlen(path) - 1] = '\0';
     }
 
-    struct Files *files = (struct Files *) malloc(sizeof(struct Files));
-    DIR *dp;
+    // check if file or directory
+    if(stat(path, &statbuf) == -1) {
+        fprintf(stderr, "stat: %s\n", path, strerror(errno));
+        return NULL;
+    }
 
+    // if dir or file
+    if(S_ISDIR(statbuf.st_mode)) {
+        return Files__create_with_dir_path(path);
+    } else {
+        return Files__create_with_file_path(path);
+    }
+}
+
+struct Files* Files__create_with_dir_path(char *dir_path) {
+    DIR *dp;
     if((dp = opendir(dir_path)) == NULL) {
-        perror("opendir");
+        fprintf(stderr, "opendir: %s\n", dir_path, strerror(errno));
         return NULL;
     }
 
     char **file_paths_buf   = (char **) malloc(sizeof(char *) * DEFAULT_FILES_SIZE);
     int file_paths_buf_size = DEFAULT_FILES_SIZE;
     int file_paths_size     = 0;
+    struct stat statbuf;
 
     struct dirent *entry;
-    struct stat   statbuf;
     while((entry = readdir(dp)) != NULL) {
         // ignore
         if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
             continue;
         }
 
-        int path_size = strlen(dir_path) * strlen(entry->d_name) + 2; // '/' と '\0' で + 2
+        // '/' と '\0' で + 2
+        int path_size = strlen(dir_path) * strlen(entry->d_name) + 2;
         char *file_path = (char *) malloc(sizeof(char) * path_size);
-        char *file_path_pos = file_path;
 
-        strncat(file_path, dir_path, strlen(dir_path));
+        strncpy(file_path, dir_path, strlen(dir_path) + 1);
         strncat(file_path, "/", strlen("/"));
         strncat(file_path, entry->d_name, strlen(entry->d_name));
 
         if(stat(file_path, &statbuf) == -1) {
-            fprintf(stderr, "stat: (%s)\n", file_path, strerror(errno));
-            Files_delete(files);
+            fprintf(stderr, "stat: %s\n", file_path, strerror(errno));
             free(file_path);
             free(file_paths_buf);
-
             return NULL;
         }
 
         if(S_ISDIR(statbuf.st_mode)) {
             free(file_path);
-
             continue;
         }
 
@@ -74,15 +89,39 @@ struct Files* Files__create(char *dir_path) {
             } else {
                 file_paths_buf = temp;
             }
-
         }
 
         file_paths_buf[file_paths_size++] = file_path;
     }
+
     closedir(dp);
 
+    struct Files *files = (struct Files *) malloc(sizeof(struct Files));
     files->file_paths = file_paths_buf;
     files->size       = file_paths_size;
+
+    return files;
+}
+
+struct Files* Files__create_with_file_path(char *path) {
+    struct Files *files = (struct Files *) malloc(sizeof(struct Files));
+
+    files->file_paths = (char **) malloc(sizeof(char *));
+    if(files->file_paths == NULL) {
+        Files_delete(files);
+        return NULL;
+    }
+
+    char *file_path = (char *) malloc(sizeof(char) * (strlen(path) + 1));
+    if(file_path == NULL) {
+        Files_delete(files);
+        return NULL;
+    }
+
+    strncpy(file_path, path, strlen(path) + 1);
+
+    files->file_paths[0] = file_path;
+    files->size          = 1;
 
     return files;
 }
--- src/main.c
+++ src/main.c
@@ -5,12 +5,13 @@
 #include "tag_array.h"
 
 int main(int argc, char** argv) {
+    // 引数の確認
     if(argc < 2) {
-        fprintf(stderr, "Argment required. %s <csv_directory_path>\n", argv[0]);
+        fprintf(stderr, "Argment required. %s <csv_or_directory_path>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    printf("%s\n", argv[1]);
+    // ファイルの取得
     struct Files *files = Files__create(argv[1]);
     if(files == NULL) {
         perror("Files__create");
@@ -20,6 +21,13 @@ int main(int argc, char** argv) {
     int i;
     for(i = 0; i < files->size; i++) {
         printf("%s\n", files->file_paths[i]);
+
+        struct TagArray *tag_array = TagArray__create_from_csv(files->file_paths[i]);
+        int j;
+        for(j = 0; j < tag_array->size; j++) {
+            printf("%s\n", tag_array->array[j]->name);
+        }
+        TagArray_delete(tag_array);
     }
 
     Files_delete(files);
